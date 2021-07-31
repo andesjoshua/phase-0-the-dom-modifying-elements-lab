4c27e9c600e8624c055507b7ff12574df6eb674d
Joshua Andes <andesjoshua@comcast.net>
2021-07-31 19:28:28 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main').remove()
+ const newHeader = document.createElement('h1');
+const createEle = document.body.appendChild(newHeader);
+newHeader.setAttribute('id', "victory")
+newHeader.innerHTML = "Josh is the champion"
